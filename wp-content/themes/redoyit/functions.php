d79a9cf75ac1749ecc696cbfbc493995f66b02fd
Redoy <mdredoyislam84@gmail.com>
2022-12-21 17:03:29 +0600

    Some Data Changes

--- wp-content/themes/redoyit/functions.php
+++ wp-content/themes/redoyit/functions.php
@@ -1,153 +1,218 @@
 <?php
-/**
- * redoyit functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package redoyit
- */
+//Include Functions
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+require_once( get_theme_file_path( "/inc/tgm.php" ) );
+require_once( get_theme_file_path( "/inc/attachments.php" ) );
+require_once( get_theme_file_path( "/widgets/social-icons-widget.php" ) );
+require_once( get_theme_file_path( "../../plugins/redux-framework" ) );
+
+if ( ! isset( $content_width ) ) {
+    $content_width = 960;
 }
 
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function redoyit_setup() {
-	/*
-		* Make theme available for translation.
-		* Translations can be filed in the /languages/ directory.
-		* If you're building a theme based on redoyit, use a find and replace
-		* to change 'redoyit' to the name of your theme in all the template files.
-		*/
-	load_theme_textdomain( 'redoyit', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-		* Let WordPress manage the document title.
-		* By adding theme support, we declare that this theme does not use a
-		* hard-coded <title> tag in the document head, and expect WordPress to
-		* provide it for us.
-		*/
-	add_theme_support( 'title-tag' );
-
-	/*
-		* Enable support for Post Thumbnails on posts and pages.
-		*
-		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		*/
-	add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'menu-1' => esc_html__( 'Primary', 'redoyit' ),
-		)
-	);
-
-	/*
-		* Switch default core markup for search form, comment form, and comments
-		* to output valid HTML5.
-		*/
-	add_theme_support(
-		'html5',
-		array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-			'style',
-			'script',
-		)
-	);
-
-	// Set up the WordPress core custom background feature.
-	add_theme_support(
-		'custom-background',
-		apply_filters(
-			'redoyit_custom_background_args',
-			array(
-				'default-color' => 'ffffff',
-				'default-image' => '',
-			)
-		)
-	);
-
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-
-	/**
-	 * Add support for core custom logo.
-	 *
-	 * @link https://codex.wordpress.org/Theme_Logo
-	 */
-	add_theme_support(
-		'custom-logo',
-		array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		)
-	);
+if ( site_url() == "http://redoyit.com" ) {
+    define( "VERSION", time() );
+} else {
+    define( "VERSION", wp_get_theme()->get( "Version" ) );
 }
-add_action( 'after_setup_theme', 'redoyit_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function redoyit_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'redoyit_content_width', 640 );
+function redoyit_theme_setup() {
+    load_theme_textdomain( "redoyit" );
+    add_theme_support( "post-thumbnails" );
+    add_theme_support( "custom-logo" );
+    add_theme_support( "title-tag" );
+    add_theme_support( 'automatic-feed-links' );
+    add_theme_support( 'html5', array( 'search-form', 'comment-list' ) );
+    add_theme_support( "post-formats", array( "image", "gallery", "quote", "audio", "video", "link" ) );
+    add_editor_style( "/assets/css/editor-style.css" );
+
+
+    register_nav_menu( "topmenu", __( "Top Menu", "redoyit" ) );
+    register_nav_menus( array(
+        "footer-left"   => __( "Footer Left Menu", "redoyit" ),
+        "footer-middle" => __( "Footer Middle Menu", "redoyit" ),
+        "footer-right"  => __( "Footer Right Menu", "redoyit" ),
+    ) );
+    add_image_size( "redoyit-home-square", 400, 400, true );
 }
-add_action( 'after_setup_theme', 'redoyit_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function redoyit_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'redoyit' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'redoyit' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
+add_action( "after_setup_theme", "redoyit_theme_setup" );
+
+function redoyit_assets() {
+    wp_enqueue_style( "fontawesome-css", get_theme_file_uri( "/assets/css/font-awesome/css/font-awesome.css" ), null, "1.0" );
+    wp_enqueue_style( "fonts-css", get_theme_file_uri( "/assets/css/fonts.css" ), null, "1.0" );
+    wp_enqueue_style( "base-css", get_theme_file_uri( "/assets/css/base.css" ), null, "1.0" );
+    wp_enqueue_style( "vendor-css", get_theme_file_uri( "/assets/css/vendor.css" ), null, "1.0" );
+    wp_enqueue_style( "main-css", get_theme_file_uri( "/assets/css/main.css" ), null, "1.0" );
+    wp_enqueue_style( "redoyit-css", get_stylesheet_uri(), null, VERSION );
+
+    wp_enqueue_script( "modernizr-js", get_theme_file_uri( "/assets/js/modernizr.js" ), null, "1.0" );
+    wp_enqueue_script( "pace-js", get_theme_file_uri( "/assets/js/pace.min.js" ), null, "1.0" );
+    wp_enqueue_script( "plugins-js", get_theme_file_uri( "/assets/js/plugins.js" ), array( "jquery" ), "1.0", true );
+    if ( is_singular() ) {
+        wp_enqueue_script( "comment-reply" );
+    }
+    wp_enqueue_script( "main-js", get_theme_file_uri( "/assets/js/main.js" ), array( "jquery" ), "1.0", true );
 }
-add_action( 'widgets_init', 'redoyit_widgets_init' );
 
-/**
- * Enqueue scripts and styles.
- */
-function redoyit_scripts() {
-	wp_enqueue_style( 'redoyit-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'redoyit-style', 'rtl', 'replace' );
+add_action( "wp_enqueue_scripts", "redoyit_assets" );
 
-	wp_enqueue_script( 'redoyit-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+function redoyit_pagination() {
+    global $wp_query;
+    $links = paginate_links( array(
+        'current'  => max( 1, get_query_var( 'paged' ) ),
+        'total'    => $wp_query->max_num_pages,
+        'type'     => 'list',
+        'mid_size' => apply_filters( "redoyit_pagination_mid_size", 3 )
+    ) );
+    $links = str_replace( "page-numbers", "pgn__num", $links );
+    $links = str_replace( "<ul class='pgn__num'>", "<ul>", $links );
+    $links = str_replace( "next pgn__num", "pgn__next", $links );
+    $links = str_replace( "prev pgn__num", "pgn__prev", $links );
+    echo wp_kses_post( $links );
 }
-add_action( 'wp_enqueue_scripts', 'redoyit_scripts' );
+
+remove_action( "term_description", "wpautop" );
+
+function redoyit_widgets() {
+    register_sidebar( array(
+        'name'          => __( 'About Us Page', 'redoyit' ),
+        'id'            => 'about-us',
+        'description'   => __( 'Widgets in this area will be shown on about us page.', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="col-block %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="quarter-top-margin">',
+        'after_title'   => '</h3>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Contact Page Maps Section', 'redoyit' ),
+        'id'            => 'contact-maps',
+        'description'   => __( 'Widgets in this area will be shown on contact page.', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '',
+        'after_title'   => '',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Contact Page Information Section', 'redoyit' ),
+        'id'            => 'contact-info',
+        'description'   => __( 'Widgets in this area will be shown on contact page.', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="col-block %2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3 class="quarter-top-margin">',
+        'after_title'   => '</h3>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Before Footer Section', 'redoyit' ),
+        'id'            => 'before-footer-right',
+        'description'   => __( 'before footer section, right side', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Footer Section', 'redoyit' ),
+        'id'            => 'footer-right',
+        'description'   => __( 'footer section, right side', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Footer Bottom Section', 'redoyit' ),
+        'id'            => 'footer-bottom',
+        'description'   => __( 'footer section, bottom side', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '',
+        'after_title'   => '',
+    ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Header Section', 'redoyit' ),
+        'id'            => 'header-section',
+        'description'   => __( 'Widgets in this area will be shown on header section.', 'redoyit' ),
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ) );
+
+}
+
+add_action( "widgets_init", "redoyit_widgets" );
+
+function category_before_title1() {
+    echo "<p>Before Title 1</p>";
+}
+
+add_action( "philosphy_before_category_title", "category_before_title1" );
+
+function category_before_title2() {
+    echo "<p>Before Title 2</p>";
+}
+
+add_action( "philosphy_before_category_title", "category_before_title2", 4 );
+
+function category_before_title3() {
+    echo "<p>Before Title 3</p>";
+}
+
+add_action( "philosphy_before_category_title", "category_before_title3", 9 );
+
+function category_after_title() {
+    echo "<p>After Title</p>";
+}
+
+add_action( "philosphy_after_category_title", "category_after_title" );
+
+function category_after_desc() {
+    echo "<p>After Description</p>";
+}
+
+add_action( "philosphy_after_category_description", "category_after_desc" );
+
+
+function beginning_category_page( $category_title ) {
+    if ( "New" == $category_title ) {
+        $visit_count = get_option( "category_new" );
+        $visit_count = $visit_count ? $visit_count : 0;
+        $visit_count ++;
+        update_option( "category_new", $visit_count );
+    }
+}
+
+add_action( "philosphy_category_page", "beginning_category_page" );
+
+
+function redoyit_home_banner_class( $class_name ) {
+    if ( is_home() ) {
+        return $class_name;
+    } else {
+        return "";
+    }
+}
+
+add_filter( "redoyit_home_banner_class", "redoyit_home_banner_class" );
+
+function pagination_mid_size( $size ) {
+    return 2;
+}
+
+add_filter( "redoyit_pagination_mid_size", "pagination_mid_size" );
+
+function uppercase_text( $param1, $param2, $param3 ) {
+    return ucwords( $param1 ) . " " . strtoupper( $param2 ) . " " . ucwords( $param3 );
+}
+
+add_filter( "redoyit_text", "uppercase_text", 10, 3 );
 
 /**
  * Implement the Custom Header feature.
@@ -168,7 +233,6 @@ require get_template_directory() . '/inc/template-functions.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
 /**
  * Load Jetpack compatibility file.
  */
@@ -182,3 +246,25 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+function redoyit_search_form( $form ) {
+    $homedir      = home_url( "/" );
+    $label        = __( "Search for:", "redoyit" );
+    $button_label = __( "Search", "redoyit" );
+    $newform      = <<<FORM
+<form role="search" method="get" class="header__search-form" action="{$homedir}">
+    <label>
+        <span class="hide-content">{$label}</span>
+        <input type="search" class="search-field" placeholder="Type Keywords" value="" name="s"
+               title="{$label}" autocomplete="off">
+    </label>
+    <input type="submit" class="search-submit" value="{$button_label}">
+</form>
+FORM;
+
+    return $newform;
+
+}
+
+add_filter( "get_search_form", "redoyit_search_form" );
+
---
+++ wp-content/themes/redoyit/inc/tgm.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once get_theme_file_path( '/lib/class-tgm-plugin-activation.php' );
+
+add_action( 'tgmpa_register', 'redoyit_register_required_plugins' );
+
+function redoyit_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		array(
+			'name'      => 'Contact Form 7',
+			'slug'      => 'contact-form-7',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Elementor',
+			'slug'      => 'elementor',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Attachments',
+			'slug'      => 'attachments',
+			'required'  => false,
+		),
+		array(
+			'name'      => 'Redux Framework',
+			'slug'      => 'redux-framework',
+			'required'  => false,
+		),
+
+	);
+	$config = array(
+		'id'           => 'redoyit',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
